ddf7d7652d702672acc3fbf3682c28e28150daac
TAYTOS <git@github.com>
2023-05-06 14:42:11 -0500

    ejercicios variables

---
+++ variables.java
@@ -0,0 +1,36 @@
+public class variables
+{
+
+	public static void main(String args[]){
+		String nombre="Taylor";
+		System.out.println(nombre);
+
+		int n=15;
+		System.out.println(n);
+
+		int m;
+		m=15;
+		System.out.println(m);
+
+		int mynum=15;
+		mynum=20;
+		System.out.println(mynum);
+		//ejercicio1
+		
+		String carname="volvo";
+		//ejercicio 2
+		int maxSpeed=120;
+		//ejercicio 3
+		int x=5;
+		int y=10;
+		System.out.println(x+y);
+		//ejercicio 4
+		int a=5;
+		int b=10;
+		int c=x+y;
+		System.out.println(c);
+		//ejercicio 5
+		int i=5,j=6,k=50;
+		System.out.println(i+j+k);
+	}
+}
