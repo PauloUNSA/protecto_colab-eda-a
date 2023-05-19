11f96f9000e94ecbff4fdf9ba262cf39b64521a3
pieroMejiaR <pmejia@unsa.edu.pe>
2023-05-19 09:06:44 -0500

    se completo el ejercicio4

---
+++ List.java
@@ -0,0 +1,29 @@
+import java.util.Collection;
+
+public interface List<E> extends Collection<E> {
+    int size();
+    boolean isEmpty();
+    boolean add(E e);
+    boolean remove(Object o);
+    boolean containsAll(Collection<?> c);
+    boolean addAll(Collection<? extends E> c);
+
+
+    boolean addAll(int index, Collection<? extends E> c);
+
+
+    boolean removeAll(Collection<?> c);
+    void clear();
+    boolean equals(Object o);
+    E get(int index);
+    E set(int index, E element);
+    void add(int index, E element);
+
+
+    E remove(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+
+
+    List<E> subList(int fromIndex, int toIndex);
+}
