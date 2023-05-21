75dd9bbc4c532e8a7bb3a53fb02a2e4939b266f0
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-21 11:33:30 -0500

    se borraron espacios en blanco

--- lab2/List.java
+++ lab2/List.java
@@ -7,23 +7,15 @@ public interface List<E> extends Collection<E> {
     boolean remove(Object o);
     boolean containsAll(Collection<?> c);
     boolean addAll(Collection<? extends E> c);
-
-
     boolean addAll(int index, Collection<? extends E> c);
-
-
     boolean removeAll(Collection<?> c);
     void clear();
     boolean equals(Object o);
     E get(int index);
     E set(int index, E element);
     void add(int index, E element);
-
-
     E remove(int index);
     int indexOf(Object o);
     int lastIndexOf(Object o);
-
-
     List<E> subList(int fromIndex, int toIndex);
 }
--- lab2/Node.java
+++ lab2/Node.java
@@ -2,27 +2,21 @@ public class Node <T>{
     private T t;
     private boolean b;
     private String s;
-
     public T getT() {
         return t;
     }
-
     public void setT(T t) {
         this.t = t;
     }
-
     public boolean isB() {
         return b;
     }
-
     public void setB(boolean b) {
         this.b = b;
     }
-
     public String getS() {
         return s;
     }
-
     public void setS(String s) {
         this.s = s;
     }
