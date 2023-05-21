6679de4e842e4cbac959ea1717e94872aa88ae71
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-21 11:00:18 -0500

    se corrigio el problema ya que era igual que el 1

--- lab2/Ejercicio2.java
+++ lab2/Ejercicio2.java
@@ -1,21 +1,25 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-        int[] A = {1,2,3,4};
+        int[] A = {1,2,3,4,5};
         for (int i :
                 A) {
             System.out.println(i);
         }
         A=rotarIzquierdaArray(A);
-
         for (int i :
                 A) {
             System.out.println(i);
         }
     }
     public static int[] rotarIzquierdaArray(int[] A){
-        int[] aiz = new int[4];
-        for (int i = 0; i < A.length; i++) {
-            aiz[i]=A[A.length-(i+1)] ;
+        int[] aiz = new int[A.length];
+        int mitad = A.length/2;
+        int k=0;
+        for (int i = mitad; i < A.length; i++,k++) {
+            aiz[k]=A[i] ;
+        }
+        for (int i = 0; i < mitad; i++,k++) {
+            aiz[k]=A[i] ;
         }
     return aiz;
 }
