03315375d5b4e9ba7b8cd70b8df4611bbb71e8cc
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-21 10:47:30 -0500

    se agregaron carpetas para ordenar los labs

--- lab2/Ejercicio2.java
+++ lab2/Ejercicio2.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Ejercicio2 {
     public static void main(String[] args) {
         int[] A = {1,2,3,4};
