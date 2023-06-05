2708fad04b9b89c7a3601193bab60075e497f4dc
Frank Villafuerte <133295276+Frank-Villafuerte@users.noreply.github.com>
2023-06-04 21:22:51 -0500

    Convertimos la clase pila en una interfaz

--- lab3/PilaImp/clases/Pila.java
+++ lab3/PilaImp/clases/Pila.java
@@ -3,54 +3,17 @@ package clases;
 import java.util.Arrays;
 import java.util.Vector;
 import exepciones.EmptyStackException;
-public class Pila<E> extends Vector{
+public interface Pila<E>{
 
-	public Pila() {
-		
-	}
 	
-	public boolean empty() {
-		return this.size()==0;
-	}
-	public E peek() throws Throwable {
-		if(this.empty()) {
-			throw(new EmptyStackException("La pila esta vacia"));
-		}
-		return  (E) this.get(this.size()-1);
-	}
-	public E pop() throws Throwable {
-		if(this.empty()) {
-			throw(new EmptyStackException("La pila esta vacia"));
-		}
-		E eliminado=(E) this.get(this.size()-1);
-		this.remove(this.get(this.size()-1));
-		return eliminado;
-	}
+	public boolean empty();
+	public E peek() throws Throwable;
+	public E pop() throws Throwable;
 	
-	public E push(E item) {
-		this.addElement(item);
-		return item;
-	}
-	public int search(Object objeto) {
-		for(int i=0;i<this.size();i++) {
-			if(objeto.equals(this.get(i))) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
+	public E push(E item) ;
+	public int search(Object objeto) throws Throwable;
 	@Override
-	public String toString() {
-		String cadena="Pila [" ;
-		for(int i=0;i<this.size();i++) {
-			if(this.get(i)!=null) {
-				cadena+=this.get(i)+" ";
-			}
-		}
-		
-		return cadena+"]";
-	}
+	public String toString();
 	
 
 }
