5ffb9cf7b36bdcf1b9a9f4fe8a65ae42d0703268
Paulo Andre Idalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-09 08:25:39 -0500

    se formateo el codigo

--- lab4/e1/Node.java
+++ lab4/e1/Node.java
@@ -1,26 +1,53 @@
-public class Node <E > {
-    Node () {
-        this . data = null ;
-        this . nextNode = null ;
-        this . previousNode = null ;
+public class Node<E> {
+    private E data;
+    private Node<E> nextNode;
+    private Node<E> previousNode;
+
+    public Node() {
+        this.data = null;
+        this.nextNode = null;
+        this.previousNode = null;
     }
-    Node ( E data ) {
-        this . data = data ;
-        this . nextNode = null ;
-        this . previousNode = null ;
+
+    public Node(E data) {
+        this.data = data;
+        this.nextNode = null;
+        this.previousNode = null;
     }
-    Node ( E data , Node <E > nextNode ) {
-        this . data = data ;
-        this . nextNode = nextNode ;
-        this . previousNode = null ;
+
+    public Node(E data, Node<E> nextNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+        this.previousNode = null;
     }
-    Node ( E data , Node <E > nextNode , Node <E > previousNode ) {
-        this . data = data ;
-        this . nextNode = nextNode ;
-        this . previousNode = previousNode ;
+
+    public Node(E data, Node<E> nextNode, Node<E> previousNode) {
+        this.data = data;
+        this.nextNode = nextNode;
+        this.previousNode = previousNode;
     }
-    private E data ;
-    private Node <E > nextNode ;
-    private Node <E > previousNode ;
-    /* Getters y Setter ... */
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<E> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public Node<E> getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(Node<E> previousNode) {
+        this.previousNode = previousNode;
+    }
 }
