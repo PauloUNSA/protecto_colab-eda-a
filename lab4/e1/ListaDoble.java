69243aaee71e620b1ca48c110fe4e3e6d47e55f2
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-10 13:44:33 -0500

    se esta implementando la clase ListaDoble

---
+++ lab4/e1/ListaDoble.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ListaDoble<E> {
+    private Node<E> inicio,fin;
+
+    public ListaDoble() {
+        inicio=fin=null;
+    }
+
+    public void addFinal(E x){
+        if(!isEmpty()){
+            fin=new Node<>(x,null,fin);
+            fin.getPreviousNode().setNextNode(fin);
+        }else {
+            inicio=fin= new Node<>(x);
+        }
+    }
+    public void addInicio(E x){
+        if(!isEmpty()){
+            inicio=new Node<>(x,inicio,null);
+            inicio.getNextNode().setPreviousNode(inicio);
+        }else {
+            inicio=fin= new Node<>(x);
+        }
+    }
+
+    public boolean isEmpty(){
+        return inicio==null;
+    }
+
+}
--- lab4/e1/Test.java
+++ lab4/e1/Test.java
@@ -1,4 +1,69 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node2 = new Node<>();
+        Node<Integer> node3 = new Node<>(1);
+        Node<Integer> node4 = new Node<>(1);
+        int tamano;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduzca el tamaño máximo del último arreglo: ");
+        tamano = teclado.nextInt();
+
+        ArrayList<int[]> casos = new ArrayList<int[]>();
+        String archivoInsercion = "insercion.txt";
+        PrintWriter oS = new PrintWriter(archivoInsercion);
+
+        for (int n = 1; n <= tamano; n++) {
+            casos.add(generarPeorCaso(n));
+        }
+        Iterator<int[]> puntero = casos.iterator();
+        while (puntero.hasNext()) {
+            oS.println(String.valueOf(insertionSort(puntero.next())));
+        }
+        oS.close();
+        // plot "/Users/richarteq/eclipse-workspace/Algoritmica/insercion.txt" with lines
+        /*JavaPlot p = new JavaPlot();
+        p.addPlot("\"/Algoritmica/insercion.txt\" with lines");
+        p.plot();*/
     }
+
+    public static long insertionSort(int[] A) {
+        int key;
+        int i;
+        long nano_startTime = System.nanoTime();
+        for (int j = 1; j < A.length; j = j + 1) {
+            key = A[j];
+//Insertar A[j] en la secuencia ordenada A[1..j-1]
+            i = j - 1;
+            while (i > -1 && A[i] > key) {
+                A[i + 1] = A[i];
+                i = i - 1;
+            }
+            A[i + 1] = key;
+//imprimirArreglo(A);
+        }
+        long nano_endTime = System.nanoTime();
+        return nano_endTime - nano_startTime;
+    }
+
+    public static int[] generarPeorCaso(int t) {
+        int[] lista = new int[t];
+        for (int i = 0; i < t; i++) {
+            lista[i] = t - i;
+        }
+        return lista;
+    }
+    public static void imprimirArreglo(int[] lista){
+        System.out.println("");
+        for (int x=0; x<lista.length; x++) {
+            System.out.print(lista[x] + " ");
+        }
+    }
+
 }
