fcef7b683082cc8bde8f562e725bf5d89a013f42
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-11 16:33:23 -0500

    se agrego las subsecciones

---
+++ lab4/latex/src/ListaDoble.java
@@ -0,0 +1,40 @@
+package e2;
+public class ListaDoble<E> {
+    private Node<E> inicio,fin;
+    public ListaDoble() {
+        inicio=fin=null;
+    }
+    public void addFinal(E x){
+        if(!isEmpty()){
+            fin=new Node<>(x,null,fin);
+            fin.getPreviousNode().setNextNode(fin);
+        }else {
+            inicio=fin= new Node<>(x);
+        }
+    }
+    public void addInicio(E x){
+        if(!isEmpty()){
+            inicio=new Node<>(x,inicio,null);
+            inicio.getNextNode().setPreviousNode(inicio);
+        }else {
+            inicio=fin= new Node<>(x);
+        }
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+    //getters y setters de inicio y fin
+    @Override
+    public String toString() {
+        String text="";
+        if (!isEmpty()){
+            Node<E> aux = inicio;
+            while (aux!=null){
+                text += aux.getData()+" ";
+                aux = aux.getNextNode();
+            }
+        }
+        return  text;
+    }
+}
