9700972ed6ea863f14abde291dbb581f5bda30cb
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-23 07:51:18 -0500

    se implemento la clase Node AVL con la clase NOde ya existente

---
+++ lab5/AVL/src/Node.java
@@ -0,0 +1,53 @@
+public class Node<E> {
+    private E data;
+    private Node<E> rigth;
+    private Node<E> left;
+
+    public Node() {
+        this.data = null;
+        this.rigth = null;
+        this.left = null;
+    }
+
+    public Node(E data) {
+        this.data = data;
+        this.rigth = null;
+        this.left = null;
+    }
+
+    public Node(E data, Node<E> rigth) {
+        this.data = data;
+        this.rigth = rigth;
+        this.left = null;
+    }
+
+    public Node(E data, Node<E> rigth, Node<E> left) {
+        this.data = data;
+        this.rigth = rigth;
+        this.left = left;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getRigth() {
+        return rigth;
+    }
+
+    public void setRigth(Node<E> rigth) {
+        this.rigth = rigth;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+}
---
+++ lab5/AVL/src/NodeAVL.java
@@ -0,0 +1,10 @@
+class NodeAVL<T> extends Node<T> {
+    protected int fe;
+    public NodeAVL(T data, NodeAVL<T> left, NodeAVL<T> right){
+        super(data, left, right);
+        this.fe = 0;
+    }
+    public NodeAVL(T data){
+        this (data, null, null);
+    }
+}
