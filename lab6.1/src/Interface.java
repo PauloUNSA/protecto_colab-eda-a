b1d96c4469cf155a93063f7c5a448a4e8719449f
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-07-26 22:23:55 -0500

    implementando interfaces

--- lab6.1/src/Interface.java
+++ lab6.1/src/Interface.java
@@ -1,4 +1,54 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 public class Interface extends JFrame {
+    JTextField texto = new JTextField(15);
+    JButton[] botones= new JButton[3];
+    JTextArea jTextArea = new JTextArea(1,1);
+    public Interface() {
+        setSize(500,500);
+        setTitle("Prueba");
+        setLayout(new BorderLayout());
+        createContents();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public void createContents(){
+        Panel panel = new Panel(new FlowLayout());
+        botones[0] = new JButton("insertar");
+        botones[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jTextArea.setText("insertar");
+            }
+        });
+        botones[1] = new JButton("buscar");
+        botones[1].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jTextArea.setText("buscar");
+            }
+        });
+        botones[2] = new JButton("reemplazar");
+        botones[2].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jTextArea.setText("reemplazar");
+            }
+        });
+        panel.add(texto);
+        panel.add(botones[0]);
+        panel.add(botones[1]);
+        panel.add(botones[2]);
+        add(panel,BorderLayout.NORTH);
+        add(jTextArea,BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        new Interface();
+    }
 
 }
--- lab6.1/src/Main.java
+++ lab6.1/src/Main.java
@@ -10,8 +10,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Trie trie = new Trie();
-        trie.insert("hola");
-        System.out.println(trie.search("hola"));
+        new Interface();
     }
 }
